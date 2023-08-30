edfd7d25c581313e526197722dab84a23f7fd38e
GladysGabriel <lgssujithra@gmail.com>
2023-08-29 21:14:30 -0700

    FirstCommit

---
+++ src/org/numbers/FindingNumbers.java
@@ -0,0 +1,14 @@
+package org.numbers;
+
+public class FindingNumbers {
+
+	public static void main(String[] args) {
+		int a=10;
+		if(a%2==0) {
+			System.out.println("A is an even number");
+		}
+		else {
+			System.out.println("A is an odd number");
+		}
+	}
+}
